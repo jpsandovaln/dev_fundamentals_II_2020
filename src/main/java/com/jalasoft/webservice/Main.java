71b634121fe02eee69368614a8f8e8a651934de7
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2020-04-27 14:46:05 -0400

    Added main class

---
+++ src/main/java/com/jalasoft/webservice/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package com.jalasoft.webservice;
+
+/**
+ * Main class.
+ *
+ * @author Paolo Sandoval
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
+}
